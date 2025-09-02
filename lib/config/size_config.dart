86b02ceccc439c616238a7a6b1322ae2e3defe67
Geovanna Silva Dias <geovanna.dias@camda.local>
2025-09-02 17:47:38 -0300

    iniciando a tela de welcome, configurando estrutura e tamanhos

--- lib/config/size_config.dart
+++ lib/config/size_config.dart
@@ -6,14 +6,17 @@ class SizeConfig {
   static late double blockWidth;
   static late double blockHeight;
 
-  static void init(BuildContext, context){
-  final size = MediaQuery.of(context).size;
+  static void init(BuildContext context) {
+    final size = MediaQuery.of(context).size;
 
-  screenHeight = size.width;
-  screenWidth = size.width;
+    screenHeight = size.height;
+    screenWidth = size.width;
 
-  blockHeight = size.height / 100;
-  blockWidth = size.width / 100;
+    blockHeight = screenHeight / 100;
+    blockWidth = screenWidth / 100;
   }
-  
+
+  // Helpers globais (podem ser usados em qualquer tela depois de chamar init)
+  static double wp(double percent) => blockWidth * percent;   // largura %
+  static double hp(double percent) => blockHeight * percent; // altura %
 }
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -4,22 +4,29 @@ import 'package:hortija_startup/config/size_config.dart';
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          child: Column(
-            children: [
-              Image.asset(
-                "assets/images/logo-hortija.png",
-                fit: BoxFit.cover,
-                width: SizeConfig.blockWidth * 0.3,
-              )
-            ],
-          ),
+@override
+Widget build(BuildContext context) {
+  SizeConfig.init(context);
+
+  return Scaffold(
+
+    body: SingleChildScrollView(
+      child: SizedBox(
+      height: SizeConfig.screenHeight,
+width: SizeConfig.screenWidth,
+        child: Column(
+mainAxisAlignment: MainAxisAlignment.spaceBetween,
+crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/images/logo-hortija.png",
+              fit: BoxFit.cover,
+              width: SizeConfig.wp(40), 
+            ),
+          ],
         ),
       ),
-    );
-  }
-}
+    ),
+  );
+}}
+
