c8481c3c2663cfb57af3d8cb2464346f7a4fd27a
Geotryy <geova.dias.gs@gmail.com>
2025-09-03 01:22:08 -0300

    tela inicial finalizada, falta apenas a rota do botão

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,32 +1,75 @@
 import 'package:flutter/material.dart';
 import 'package:hortija_startup/config/size_config.dart';
+import 'package:hortija_startup/theme/colors.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
 
-@override
-Widget build(BuildContext context) {
-  SizeConfig.init(context);
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig.init(context);
 
-  return Scaffold(
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: SizeConfig.screenHeight,
+          width: SizeConfig.screenWidth,
+          child: Padding(
+            padding: EdgeInsetsDirectional.symmetric(
+                horizontal: SizeConfig.wp(8), vertical: 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/images/logo-hortija.png",
+                  fit: BoxFit.cover,
+                  width: SizeConfig.wp(60),
+                ),
 
-    body: SingleChildScrollView(
-      child: SizedBox(
-      height: SizeConfig.screenHeight,
-width: SizeConfig.screenWidth,
-        child: Column(
-mainAxisAlignment: MainAxisAlignment.spaceBetween,
-crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset(
-              "assets/images/logo-hortija.png",
-              fit: BoxFit.cover,
-              width: SizeConfig.wp(40), 
+                Column(children: [Center(
+                  child: Text(
+                    "Mais negócio, menos viagem perdida",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: AppColors.primaryOrange,
+                        fontWeight: FontWeight.bold,
+                        fontSize: SizeConfig.hp(4)),
+                  ),
+                ),
+                SizedBox(height: SizeConfig.hp(2),),
+                Center(
+                  child: Text(
+                    "Aqui você encontra produtores verificados, compra com segurança e otimiza sua rota de coleta.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: AppColors.lightGreen,
+                        fontSize: SizeConfig.hp(2)),
+                  ),
+                ),],),
+                
+                SizedBox(
+                  width: SizeConfig.screenWidth,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: AppColors.primaryGreen),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Text(
+                        'Iniciar',
+                        style: TextStyle(
+                            color: Colors.white, fontSize: SizeConfig.hp(2)),
+                      ),
+                    ),
+                  ),
+                )
+              ],
             ),
-          ],
+          ),
         ),
       ),
-    ),
-  );
-}}
-
+    );
+  }
+}
--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -1,5 +1,11 @@
-class AppColors{
+import 'dart:ui';
 
+class AppColors{
+  static const Color background =  Color(0XFFFCF6DE);
+  static const Color primaryGreen =  Color(0XFF026838);
+  static const Color lightGreen =  Color(0XFF026838);
+  static const Color primaryOrange =  Color(0XFFEF7A2D);
+  static const Color cardBG =  Color(0XFFE7E0C0);
 
 
 }
